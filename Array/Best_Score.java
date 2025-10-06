d48d8f54635715a92d714c04786ed267f6a797ff
Golla Jyothika <jyothikag0702@gmail.com>
2025-10-07 00:19:54 +0530

    Add function to find top two scores from array
    Implemented a function to find the first and second highest scores in an array, handling duplicates.

---
+++ Array/Best_Score.java
@@ -0,0 +1,27 @@
+//Best Score
+//Given an array, write a function to get first, second best scores from the array and return it in new array.
+//Array may contain duplicates.
+//Example
+//myArray = {84,85,86,87,85,90,85,83,23,45,84,1,2,0}
+//firstSecond(myArray) // {90, 87}
+
+import java.util.Arrays;
+import java.util.Collections;
+public class Best_Score {
+    public static int[] findTopTwoScores(int[] array) {
+        int firstHighest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+ 
+        for (int score : array) {
+            if (score > firstHighest) {
+                secondHighest = firstHighest;
+                firstHighest = score;
+            } else if (score > secondHighest && score < firstHighest) {
+                secondHighest = score;
+            }
+        }
+ 
+        return new int[]{firstHighest, secondHighest};
+    }
+}
+// Time Complexity: O(n)
